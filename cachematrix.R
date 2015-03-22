defcea4e26d5842f99df83739eda6cb9f3b9c3e6
ciarankohlilynch <c.kohli-lynch.1@research.gla.ac.uk>
2015-03-22 21:13:32 +0000

    Programming Assignment 2
    For peer review

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse<-NULL
+  #sets value of the matrix
+  set<-function(y){
+    x<<-y
+    inverse<<-NULL
+  }
+  #gets value of the matrix
+  get<-function() x
+  
+  #sets value of inverse 
+  setInverse<-function(solve) m<<- solve
+  
+  #gets value of inverse
+  getInverse<-function() m
+  list(set=set, get=get,
+       setInverse=setInverse,
+       getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #checks if inverse already computed
+  inv<-x$getInverse()
+  #returns inverse if already computed
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  #computes inverse if not computed
+  matrix<-x$get()
+  inverse<-solve(matrix, ...)
+  x$setInverse(inv)
+  #returns computed inverse
+  inv
 }
